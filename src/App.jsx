382228f54c48cb2264d81a8ea724802b4d78d4ef
Jessica Cousins <jcousins87@gmail.com>
2024-09-11 18:26:45 -0400

    rules styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,53 @@
+import React, { useState } from "react";
 import BlackjackGame from "./components/BlackjackGame";
 import "./App.css";
 
 function App() {
+  const [showRules, setShowRules] = useState(false);
+
+  const toggleRules = () => {
+    setShowRules((prevState) => !prevState);
+  };
+
   return (
     <div className="App">
+      <button className="toggle-rules-button" onClick={toggleRules}>
+        {showRules ? "Hide Rules" : "Show Rules"}{" "}
+      </button>
+
+      {showRules && (
+        <div className="rules">
+          <h2>Blackjack Rules</h2>
+          <p>
+            The goal of Blackjack is to beat the dealer's hand without going
+            over 21.
+          </p>
+          <ul>
+            <li>
+              <span className="emoji">♠️</span> Each player starts with two
+              cards, one of the dealer's cards is hidden until the end.
+            </li>
+            <li>
+              <span className="emoji">♥️</span> To 'Hit' is to ask for another
+              card. To 'Stand' is to hold your total and end your turn.
+            </li>
+            <li>
+              <span className="emoji">♦️</span> If you go over 21, you bust, and
+              the dealer wins regardless of the dealer's hand.
+            </li>
+            <li>
+              <span className="emoji">♣️</span> If you are dealt 21 from the
+              start (Ace & 10), you got a Blackjack!
+            </li>
+            <li>
+              <span className="emoji">♠️</span> Dealer will hit until their
+              cards total 17 or higher.
+            </li>
+          </ul>
+        </div>
+      )}
+
       <BlackjackGame />
-      <div className="rules">
-        <h2>Blackjack Rules</h2>
-        <p>
-          The goal of Blackjack is to beat the dealer's hand without going over
-          21.
-        </p>
-        <ul>
-          <li>
-            <span className="emoji">♠️</span> Each player starts with two cards,
-            one of the dealer's cards is hidden until the end.
-          </li>
-          <li>
-            <span className="emoji">♥️</span> To 'Hit' is to ask for another
-            card. To 'Stand' is to hold your total and end your turn.
-          </li>
-          <li>
-            <span className="emoji">♦️</span> If you go over 21, you bust, and
-            the dealer wins regardless of the dealer's hand.
-          </li>
-          <li>
-            <span className="emoji">♣️</span> If you are dealt 21 from the start
-            (Ace & 10), you got a Blackjack!
-          </li>
-          <li>
-            <span className="emoji">♠️</span> Dealer will hit until their cards
-            total 17 or higher.
-          </li>
-        </ul>
-      </div>
     </div>
   );
 }
